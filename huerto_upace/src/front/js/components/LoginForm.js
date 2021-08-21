c13f1f30431f9b9430a2df0b49bed57a5cfa95da
Alvarodevs <alvaro.develops@gmail.com>
2021-08-21 02:09:58 +0200

    Layout in login & register done, storing userData object, DNS_PROBE_FINISHED_NXDOMAIN not fixed on @auth0

--- huerto_upace/src/front/js/components/LoginForm.js
+++ huerto_upace/src/front/js/components/LoginForm.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from "prop-types";
 import { Modal, Button, Form } from 'react-bootstrap';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Link } from 'react-router-dom';
 
 export const Login = (props) => {
+    console.log(props)
+    const [show, setShow] = useState("");
+    
+    useEffect(() => {
+        setShow(props.show);
+      }, [props]);
 
     const { loginWithRedirect } = useAuth0();
 
@@ -13,11 +19,16 @@ export const Login = (props) => {
         <div>
             <Modal
                 className="modal-login"
-                show={props.show}
+                show={show}
                 onHide={props.onHide}
             >
                 <Form className="form-container">
+                    <Button className="close-button justify-content-end" onClick={props.onHide}>
+                       <img src=".\images\window-close-solid.svg"></img>
+                    </Button>
+
                     <h3 className="mb-3"> Accede con tu usuario</h3>
+
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Control type="email" placeholder="Enter email" />
                     </Form.Group>
@@ -25,27 +36,30 @@ export const Login = (props) => {
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Control type="password" placeholder="Password" />
                     </Form.Group>
-                    <div className="d-flex flex-row justify-content-between mb-3">
+
+                    <div className="d-flex flex-row justify-content-end mb-3">
                         
-                            <Form.Check type="checkbox" label="Recuérdame" />
+                            {/* <Form.Check type="checkbox" label="Recuérdame" /> */}
                             <Form.Text className="text-muted">
                             ¿No tienes cuenta?
                             </Form.Text>
                     </div>
+
                     <div className="modal-buttons-container">
                         <div>
-                            <Button variant="primary" type="submit">
+                            <Button type="submit" onClick={() => loginWithRedirect()}>
                             Login
-                            
                             </Button>
                         </div>
+
                         <div>
-                            <Button variant="primary" type="submit" onClick={() => loginWithRedirect()}>
+                            <Button type="submit" onClick={props.onHide}>
                                 <Link to="/registration">
                                 Regístrate
                                 </Link>
                             </Button>
                         </div>
+
                     </div>
                 </Form>
             </Modal>
